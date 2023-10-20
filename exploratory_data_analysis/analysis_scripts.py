d632aec62244fdafc0f57a2854efc7c625570798
chdorgeix1 <112966997+chdorgeix1@users.noreply.github.com>
2023-10-20 14:20:44 -0400

    Added and Implemented Contrast Examination Functions

--- exploratory_data_analysis/analysis_scripts.py
+++ exploratory_data_analysis/analysis_scripts.py
@@ -47,3 +47,38 @@ def show_bright_and_dim_imgs(bright_list, img_list):
 
     plt.tight_layout()  # Adjust spacing between subplots
     plt.show()
+    
+    
+def contrast_hist_for_img_folder(directory):
+    img_list = create_img_list(directory)
+    
+    avg_contrast_list = []
+
+    for img in img_list:
+        img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        avg_contrast_list.append(img_grey.std())
+    
+    plt.hist(avg_contrast_list, bins = 10)
+    plt.show()
+    
+    return(img_list, avg_contrast_list)
+
+def show_high_low_contrast_imgs(contrast_list, img_list):
+    # Create a subplot with 1 row and 2 columns
+    plt.figure(figsize=(6, 3))  # Adjust the figure size as needed
+    
+    x1 = int(np.argsort(contrast_list)[-1:])
+    x2 = int(np.argsort(contrast_list)[:1])
+    
+    # Plot the first image in the first subplot
+    plt.subplot(1, 2, 1)
+    plt.imshow(img_list[x1])
+    plt.title('High Contrast Image')
+
+    # Plot the second image in the second subplot
+    plt.subplot(1, 2, 2)
+    plt.imshow(img_list[x2])
+    plt.title('Low Contrast Image')
+
+    plt.tight_layout()  # Adjust spacing between subplots
+    plt.show()
