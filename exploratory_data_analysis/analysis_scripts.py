acd803b02c4053795b5d80b707d244e33637134e
chdorgeix1 <112966997+chdorgeix1@users.noreply.github.com>
2023-10-20 14:12:35 -0400

    Added and Implemented Brightness Distr. Functions

--- exploratory_data_analysis/analysis_scripts.py
+++ exploratory_data_analysis/analysis_scripts.py
@@ -5,8 +5,6 @@ import random
 import cv2
 import numpy as np
 
-
-
 def create_img_list(directory):
     os.chdir(directory)
     file_list = os.listdir()
@@ -16,3 +14,36 @@ def create_img_list(directory):
         img_list.append(img1)
     return(img_list)
     
+def brightness_hist_for_img_folder(directory):
+    img_list = create_img_list(directory)
+    
+    avg_bright_list = []
+
+    for img in img_list:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        avg_bright_list.append(img[..., 2].mean())
+
+    plt.hist(avg_bright_list, bins = 10)
+    plt.show()
+    return(img_list, avg_bright_list)
+
+
+def show_bright_and_dim_imgs(bright_list, img_list):
+    # Create a subplot with 1 row and 2 columns
+    plt.figure(figsize=(6, 3))  # Adjust the figure size as needed
+    
+    x1 = int(np.argsort(bright_list)[-1:])
+    x2 = int(np.argsort(bright_list)[:1])
+    
+    # Plot the first image in the first subplot
+    plt.subplot(1, 2, 1)
+    plt.imshow(img_list[x1])
+    plt.title('Bright Image')
+
+    # Plot the second image in the second subplot
+    plt.subplot(1, 2, 2)
+    plt.imshow(img_list[x2])
+    plt.title('Dim Image')
+
+    plt.tight_layout()  # Adjust spacing between subplots
+    plt.show()
