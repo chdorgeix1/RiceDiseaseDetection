720d598628c3d7623c61978a56b93d7828da9a81
chdorgeix1 <112966997+chdorgeix1@users.noreply.github.com>
2023-12-29 14:27:01 -0500

    Created Utility class for image preprocessing

--- training_ML_models/image_preprocessing_scripts.py
+++ training_ML_models/image_preprocessing_scripts.py
@@ -2,35 +2,39 @@ import os
 import cv2
 import numpy as np
 
-def import_datasets(main_directory, image_directories):
-    img_lists = [[],[],[],[]]
-    for img_dir in enumerate(image_directories):
-        os.chdir(main_directory + img_dir[1])
-        for img in os.listdir(main_directory + img_dir[1]):
-            img1 = cv2.imread(img)
-            img_lists[img_dir[0]].append(img1)
-        os.chdir('..')
-    return img_lists
 
+class Utils:
+    
+    @staticmethod
+    def import_datasets(main_directory, image_directories):
+        img_lists = [[],[],[],[]]
+        for img_dir in enumerate(image_directories):
+            os.chdir(main_directory + img_dir[1])
+            for img in os.listdir(main_directory + img_dir[1]):
+                img1 = cv2.imread(img)
+                img_lists[img_dir[0]].append(img1)
+            os.chdir('..')
+        return img_lists
 
-def create_label_array(datasets_list):
-    label_array = np.asarray([])
-    for i in enumerate(datasets_list):
-        label_array = np.append(label_array, np.full((len(i[1]),), i[0]))
-    return label_array
+    @staticmethod
+    def create_label_array(datasets_list):
+        label_array = np.asarray([])
+        for i in enumerate(datasets_list):
+            label_array = np.append(label_array, np.full((len(i[1]),), i[0]))
+        return label_array
 
+    @staticmethod
+    def combine_lists(datasets_list):
+        single_list = []
+        for i in datasets_list:
+            single_list += i
+        return single_list
 
-def combine_lists(datasets_list):
-    single_list = []
-    for i in datasets_list:
-        single_list += i
-    return single_list
-
-
-def lists_to_array(combined_list):
-    combined_arr = np.asarray(combined_list)
-    return combined_arr
-
-
-def convert_pixel_values(input_arr):
-    return input_arr/255
+    @staticmethod
+    def lists_to_array(combined_list):
+        combined_arr = np.asarray(combined_list)
+        return combined_arr
+    
+    @staticmethod
+    def convert_pixel_values(input_arr):
+        return input_arr/255
